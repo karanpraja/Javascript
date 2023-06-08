200787bf79f6bf0c9e4bebda43ec4780bcfc5b3a
Karan <karanpraja902@gmail.com>
2023-06-08 21:35:11 +0530

    add-fizzbuzz

---
+++ Javascript/fizzbuzz.js
@@ -0,0 +1,22 @@
+var output=[];
+var count=1;
+function fizzbuzz(){//use return statement in this to avoid errors
+    output.push(count);
+    count++;
+    var i=(output.length)-1;
+    var x="fizz";
+    var y="buzz";
+    if(output[i]%3===0)
+    {
+      if(output[i]%5==0)
+      {
+          return (x+y);
+       }
+        return x;
+    }
+    if(output[i]%5===0)
+    {
+   return y;
+    }
+    console.log(output);
+}
